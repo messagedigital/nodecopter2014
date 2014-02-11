58a530f8ebca408c15c8e33742384c9036a33e99
Joe Holdcroft <joe@message.co.uk>
2014-02-11 21:15:33 +0000

    Added basic leap motion controller for altitude

---
+++ tests/leapmotion-server/server.js
@@ -0,0 +1,56 @@
+var Leap    = require('leapjs');
+var net     = require('net');
+var port    = 1337;
+var clients = [];
+var server  = net.createServer(function(socket) {
+  // Add the client to the array of clients
+  var i = clients.push(socket);
+
+  // Remove the client from the clients array when they bail
+  socket.on('end', function() {
+    clients.splice(i - 1, 1);
+  });
+}).listen(port);
+
+var altitudeChangeThreshold = 10;
+var altitude = null;
+var lastSampledAltitude = null;
+
+Leap.loop({}, function(frame) {
+  var hands = frame.hands;
+
+  // Look ma, no hands!
+  if (hands.length < 1) {
+    return;
+  }
+
+  // Get the first hand
+  var hand = hands[0];
+
+  // Get the hand altitude
+  var pos  = hand.palmPosition;
+
+  altitude = pos[1];
+});
+
+// Sample the altitude every 100 milliseconds
+setInterval(function() {
+  var change = -1 * Math.round((lastSampledAltitude - altitude) * 0.05);
+
+  if (0 == change) {
+    return;
+  }
+
+  console.log(change);
+
+  write(change);
+
+  lastSampledAltitude = altitude;
+}, 200);
+
+// Send altitude to all clients
+function write(msg) {
+  clients.forEach(function(client) {
+    client.write(String(msg));
+  });
+}
