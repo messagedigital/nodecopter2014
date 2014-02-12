232b018bc477ccf8a92989ca33b2be24e0aef938
Laurence Roberts <lsjroberts@gmail.com>
2014-02-12 13:55:35 +0000

    Updated oculus video to use an arbitrary image stream

--- public/oculus.js
+++ public/oculus.js
@@ -8,6 +8,7 @@ Oculus = (function() {
 		this.effect   = null;
 		this.state    = null;
 		this.rotationChangeCallback = null;
+		this.videoImageSource = null;
 	}
 
 	Oculus.prototype.detect = function(callback) {
@@ -78,7 +79,22 @@ Oculus = (function() {
 		}
 	}
 
-	Oculus.prototype.showVideo = function() {
+	Oculus.prototype.updateVideoFrame = function(image) {
+		self.videoImageSource = image;
+	};
+
+	Oculus.prototype.getVideoFromWebCam = function() {
+		navigator.webkitGetUserMedia({video: true, audio: false}, function(stream) {
+			// Create the video element and grab the src from the webcam stream
+			video = document.createElement('video');
+			video.src = window.URL.createObjectURL(stream);
+			video.play();
+
+			self.videoImageSource = video;
+		});
+	}
+
+	Oculus.prototype.initVideo = function() {
 		var self = this;
 
 		var camera, scene, renderer,
@@ -95,11 +111,6 @@ Oculus = (function() {
 		animate();
 
 		function init() {
-			createRenderer();
-			getWebcam();
-		}
-
-		function createRenderer() {
 			// Create a camera
 			camera = new THREE.PerspectiveCamera(fov, window.innerWidth / window.innerHeight, 1, 1000);
 
@@ -121,45 +132,36 @@ Oculus = (function() {
 
 			// Add renderer to document
 			document.body.appendChild(renderer.domElement);
-		}
 
-		function getWebcam() {
-			navigator.webkitGetUserMedia({video: true, audio: false}, function(stream) {
-				// Create the video element and grab the src from the webcam stream
-				video = document.createElement('video');
-				video.src = window.URL.createObjectURL(stream);
-				video.play();
+			// Create a canvas for the video
+			videoImage = document.createElement('canvas');
+			videoImage.width = videoWidth;
+			videoImage.height = videoHeight;
 
-				// Create a canvas for the video
-				videoImage = document.createElement('canvas');
-				videoImage.width = videoWidth;
-				videoImage.height = videoHeight;
+			// Blank out the canvas if there is no video
+			videoImageContext = videoImage.getContext('2d');
+			videoImageContext.fillStyle = '#000000';
+			videoImageContext.fillRect(0, 0, videoImage.width, videoImage.height);
 
-				// Blank out the canvas if there is no video
-				videoImageContext = videoImage.getContext('2d');
-				videoImageContext.fillStyle = '#000000';
-				videoImageContext.fillRect(0, 0, videoImage.width, videoImage.height);
+			// Create a texture from the video image
+			videoTexture = new THREE.Texture(videoImage);
+			videoTexture.minFilter = THREE.LinearFilter;
+			videoTexture.magFilter = THREE.LinearFilter;
 
-				// Create a texture from the video image
-				videoTexture = new THREE.Texture(videoImage);
-				videoTexture.minFilter = THREE.LinearFilter;
-				videoTexture.magFilter = THREE.LinearFilter;
+			// Create a material and geometry and apply them to a mesh
+			videoMaterial = new THREE.MeshBasicMaterial({
+				map: videoTexture,
+				overdraw: true
+			})
+			videoGeometry = new THREE.PlaneGeometry(240, videoHeight * (240 / videoWidth), 4, 4);
+			videoScreen = new THREE.Mesh(videoGeometry, videoMaterial);
+			videoScreen.position.set(0, 0, 0);
 
-				// Create a material and geometry and apply them to a mesh
-				videoMaterial = new THREE.MeshBasicMaterial({
-					map: videoTexture,
-					overdraw: true
-				})
-				videoGeometry = new THREE.PlaneGeometry(240, videoHeight * (240 / videoWidth), 4, 4);
-				videoScreen = new THREE.Mesh(videoGeometry, videoMaterial);
-				videoScreen.position.set(0, 0, 0);
+			// Add the video screen mesh to the scene and point the camera at it
+			scene.add(videoScreen);
 
-				// Add the video screen mesh to the scene and point the camera at it
-				scene.add(videoScreen);
-
-				camera.position.set(0, 0, 100);
-				camera.lookAt(videoScreen.position);
-			});
+			camera.position.set(0, 0, 100);
+			camera.lookAt(videoScreen.position);
 		}
 
 		function animate() {
@@ -173,7 +175,15 @@ Oculus = (function() {
 		}
 
 		function render()  {
-			if (video && video.readyState === video.HAVE_ENOUGH_DATA) {
+			if (! self.videoImageSource) return;
+
+			videoImageContext.drawImage(self.videoImageSource, 0, 0);
+
+			if (videoTexture) {
+				videoTexture.needsUpdate = true;
+			}
+
+			/*if (video && video.readyState === video.HAVE_ENOUGH_DATA) {
 				videoImageContext.drawImage(video, 0, 0);
 
 				// Apply filter
@@ -183,7 +193,7 @@ Oculus = (function() {
 				if (videoTexture) {
 					videoTexture.needsUpdate = true;
 				}
-			}
+			}*/
 
 			var polled = vr.pollState(self.state);
 			riftEffect.render(scene, camera, polled ? self.state : null);
